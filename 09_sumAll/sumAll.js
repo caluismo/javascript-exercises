bc5a2c2989817dabe81d0273cda23516c4e66b33
Luis <232109504+caluismo@users.noreply.github.com>
2025-10-12 17:16:31 -0400

    Complete ex 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(aNum, bNum) {
+    if (Number.isInteger(aNum) && aNum > 0 &&
+        Number.isInteger(bNum) && bNum > 0) {
+            let min = Math.min(aNum, bNum);
+            let max = Math.max(aNum, bNum);
+            let sum = 0;
 
+            for (let i = min; i <= max; i++){
+                sum += i;
+        }
+        return sum;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
