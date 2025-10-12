10fd1a434d3d1b49e4e852be4d469da652d3657c
Luis <232109504+caluismo@users.noreply.github.com>
2025-10-12 16:57:02 -0400

    Complete ex 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,4 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...toRemove) => arr.filter(item => !toRemove.includes(item));
 
 // Do not edit below this line
 module.exports = removeFromArray;
