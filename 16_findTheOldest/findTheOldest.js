b6d0a24992bbb16233c643767e8f180c98536a27
Luis <232109504+caluismo@users.noreply.github.com>
2025-10-28 14:10:56 -0400

    Complete ex 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+    // handle missing yearOfDeath by using current year
+    const currentYear = new Date().getFullYear();
 
+    const oldAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    const newAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    return newAge > oldAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
