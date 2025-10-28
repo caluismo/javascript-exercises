5fe37de78ff70078153d9a1c64be32a20d105ebe
Luis <232109504+caluismo@users.noreply.github.com>
2025-10-28 13:44:34 -0400

    Complete ex 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, ''); 
 
+    const reversed = phrase.split('').reverse().join('');
+    return phrase === reversed;
 };
 
 // Do not edit below this line
